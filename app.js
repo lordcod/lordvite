89ad905de5f47c9b5eff5735a14995d9f2a664d9
LordCode <9999269010dddd@gmail.com>
2024-06-09 02:05:09 +0300

    ww

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const nextPath = path.join(__dirname, 'node_modules', '.bin', 'vite');
 process.argv.length = 0;
 process.argv.push(nextPath);
 process.env.NODE_ENV = 'production';
+
 require(nextPath);
